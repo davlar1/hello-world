982aab8155f652dfee60d5290af34192f1389fd4
David Ramble <david.ramble@cambridgeconsultants.com>
2019-07-16 18:57:45 +0100

    add to branch feature_1

---
+++ module1.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "module1.h"
+
+
+void func1(void)
+{
+    int a = 0;
+
+    /* Do some stuff */
+    a = a + 1;
+    
+}
---
+++ module1.h
@@ -0,0 +1,3 @@
+#include "stdio.h"
+
+static void func1(void);
